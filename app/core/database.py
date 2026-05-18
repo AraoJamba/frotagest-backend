d194a5d8eda055ff68263d80a95c13ed4c5550e3
AraoJamba <wefl3xxx@gmail.com>
2026-05-18 12:02:02 +0100

    fix database url

--- app/core/database.py
+++ app/core/database.py
@@ -1,4 +1,36 @@
+
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL não encontrada")
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+"""from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 import os
@@ -26,3 +58,5 @@ def get_db():
         yield db
     finally:
         db.close()
+
+"""
